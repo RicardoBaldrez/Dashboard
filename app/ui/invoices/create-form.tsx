1aa7d13b9e3905c2260e928fe92c3e8f454b2634
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2024-04-19 17:35:32 -0300

    Improving accessibility(edit-form)

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -17,7 +17,6 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState = { message: null, errors: {} };
 
   const [state, dispatch] = useFormState(createInvoice, initialState);
-  console.log(state);
 
   return (
     <form action={dispatch}>
@@ -125,14 +124,17 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 </label>
               </div>
             </div>
-            <div id="amount-error" aria-live="polite" aria-atomic="true">
-              {state.errors?.status &&
-                state.errors.status.map((error: string) => (
-                  <p className="mt-2 text-sm text-red-500" key={error}>
-                    {error}
-                  </p>
-                ))}
-            </div>
+          </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status &&
+              state.errors.status.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+          <div aria-live="polite" aria-atomic="true">
+            {state?.message && <p className="mt-2 text-sm text-red-500">{state.message}</p>}
           </div>
         </fieldset>
       </div>
